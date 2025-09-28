331e975fa02f5960941492e9b8645a3330521c5a
Lokesh Hans <Lokeshbaghelhans@gmail.com>
2025-09-28 12:35:16 +0530

    add Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import './Navbar.css'
 function Navbar() {
   return (
     <>
-    navbar
+    <div className='navbar-wrapper'>
+        <div className="heading">
+            Movie Base
+        </div>
+        <div className="search-box">
+            <input type="text"
+            placeholder='Search Novie by name....' />
+        </div>
+        <div className="theme">
+            <button>Dark</button>
+        </div>
+    </div>
     </>
   )
 }
