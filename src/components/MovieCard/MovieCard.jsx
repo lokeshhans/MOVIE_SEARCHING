2cb40d7479749bc8c4a38088772885f9641b8377
Your Name <hanswork@gmail.com>
2025-09-29 08:52:23 +0530

    Adding the Movie Component

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./MovieCard.css";
+function MovieCard({ Title, Year, Type, Poster }) {
+  return (
+    <>
+      <div className="movie-wrapper">
+        <div className="movie-img">
+          <img src={Poster} alt="" />
+        </div>
+        <div className="movie-title">
+          <span>{Title}</span>
+        </div>
+        <div className="movie-year">
+          <span>Realease in: {Year}</span>
+        </div>
+        <div className="movie-type">
+          <span>{Type}</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MovieCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-
+import MovieCard from '../components/MovieCard/MovieCard'
+import './Home.css'
 function Home() {
   return (
     <>
     {/* navbar */}
     {/*Movie List */}
+    <div className="movie-card-wrapper">
+      <MovieCard Title={"Harry Potter and the Deathly Hallows: Part 2"} Year={2020} Type={"movie"} Poster={"https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_SX300.jpg"}/>
+      <MovieCard Title={"Harry Potter and the Deathly Hallows: Part 2"} Year={2020} Type={"movie"} Poster={"https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_SX300.jpg"}/>
+      <MovieCard Title={"Harry Potter and the Deathly Hallows: Part 2"} Year={2020} Type={"movie"} Poster={"https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_SX300.jpg"}/>
+    </div>
     {/*Pagination Buttons */}
 </>
 )
