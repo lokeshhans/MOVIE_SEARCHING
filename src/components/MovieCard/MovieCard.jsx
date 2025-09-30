16295063c17ab30790d260c47af0bdfa3eae56d1
Lokesh <lokeshbaghelhans@gmail.com>
2025-09-30 21:51:55 +0530

    Adding the movie details

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,8 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import "./MovieCard.css";
-function MovieCard({ Title, Year, Type, Poster }) {
+function MovieCard({ Title, Year, Type, Poster,imdbID }) {
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate(`/movie/${imdbID}`);
+  }
   return (
     <>
-      <div className="movie-wrapper">
+      <div className="movie-wrapper" onClick={handleClick}>
         <div className="movie-img">
           <img src={Poster} alt="" />
         </div>
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import "./MovieDetail.css";
 import { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 import { MovieSearchById } from "../apis/MovieSearch";
@@ -21,16 +22,29 @@ function MovieDetail() {
   },[id]);
 
   return <>
-  
-    {movie && 
-      < MovieCard 
-        Title={movie.Title} 
-        Year={movie.Year} 
-        Poster={movie.Poster} 
-        Type={movie.Type}
-      />
-    }
-  </>;
+    <div className="movie-detail-page">
+        {movie && 
+          < MovieCard 
+            Title={movie.Title} 
+            Year={movie.Year} 
+            Poster={movie.Poster} 
+            Type={movie.Type}
+          />
+        }
+      <div className="movie-detail">
+        <span>Movie Details</span> <br />
+        Plot: {movie?.Plot} <br />
+        Director: {movie?.Director} <br />
+        Actors: {movie?.Actors} <br />
+        Writer: {movie?.Writer} <br />
+        Year: {movie?.Year} <br />
+        Released: {movie?.Released} <br />
+        Runtime: {movie?.Runtime} <br />
+        Genre: {movie?.Genre} <br />  
+        Awards: {movie?.Awards} <br />
+      </div>
+    </ div>;
+  </>
 }
 
 export default MovieDetail;
